0b4f2c74f7dba6aba22e5392abe478a333ff408d
500 <git.hub@myrk.eu>
2022-06-27 13:37:06 +0300

    added NullPointerException to resolve error that was popping up.

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         try (ServerSocket serverSocket = new ServerSocket(8080)) {
-            System.out.println(" server käivitatud\n ootame uusi sõnumeid");
+            System.out.println(" lihtne HTTP server käivitatud \n ootame uusi sõnumeid");
 
             while (true) {
 
@@ -17,33 +17,32 @@ public class Main {
 //                    System.out.println("Debug: tuli uus sõnum_ " + client.toString());
 
                     InputStreamReader inputStreamReader = new InputStreamReader(client.getInputStream());
-
                     BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
 
                     StringBuilder request = new StringBuilder();
+
                     String line;
-                    line = bufferedReader.readLine();
+                    line = bufferedReader.readLine();           // loeme puhvrist rida haaval
                     while (!line.isBlank()) {
-                        request.append(line).append("\r\n");
+                        request.append(line).append("\r\n");    // lisame objekti REQUEST sisse line sisu + reavahetus \r\n return carriage/newline
                         line = bufferedReader.readLine();
                     }
-                    String firstLine = String.valueOf(request.toString().split("\n")[0]);
-                    String resource = firstLine.split(" ")[1];
-//                    System.out.println(" resurss " +resource);
+
+                    String resource = getResourceString(request);
 
 
                     if (resource.equals("/cat")) {
 //                        System.out.println(resource.equals("/cat"));
                         FileInputStream image = new FileInputStream("me.jpg");
                         OutputStream clientOutput = client.getOutputStream();
-                        clientOutput.write(("HTTP/1.1 200 OK}\r\n").getBytes());
+                        clientOutput.write(("HTTP/1.0 200 OK}\r\n").getBytes());
                         clientOutput.write(("\r\n").getBytes());
 //                        clientOutput.write("<head> <link rel=\"icon\" href=\"data:,\"> </head>\r\n".getBytes());  // https://stackoverflow.com/questions/1321878/how-to-prevent-favicon-ico-requests
                         clientOutput.write(image.readAllBytes());
                         clientOutput.flush();
                     } else if (resource.equals("/hello")) {
                         OutputStream clientOutput = client.getOutputStream();
-                        clientOutput.write(("HTTP/1.1 200 OK}\r\n").getBytes());
+                        clientOutput.write(("HTTP/1.0 200 OK}\r\n").getBytes());
                         clientOutput.write(("\r\n").getBytes());
                         clientOutput.write("<head> <link rel=\"icon\" href=\"data:,\"> </head>\r\n".getBytes());
                         clientOutput.write(("See on Hello World!\r\n").getBytes());
@@ -51,7 +50,7 @@ public class Main {
                     } else if (resource.equals("/")) {
                         FileInputStream fileInputStream = new FileInputStream("index.html");
                         OutputStream clientOutput = client.getOutputStream();
-                        clientOutput.write(("HTTP/1.1 200 OK}\r\n").getBytes());
+                        clientOutput.write(("HTTP/1.0 200 OK}\r\n").getBytes());
                         clientOutput.write(("\r\n").getBytes());
                         clientOutput.write("<head> <link rel=\"icon\" href=\"data:,\"> </head>\r\n".getBytes());
                         clientOutput.write(fileInputStream.readAllBytes());
@@ -59,20 +58,31 @@ public class Main {
                         // TODO serveerib indexi, aga gallery.html kuvamisega jääb hätta. uurib mida ja kuidas edasi vaja teha, et teiste failide serveerimine tööle saada.
                     } else {
                         OutputStream clientOutput = client.getOutputStream();
-                        clientOutput.write(("HTTP/1.1 200 OK}\r\n").getBytes());
+                        clientOutput.write(("HTTP/1.1 404 Not Found}\r\n").getBytes());
                         clientOutput.write(("\r\n").getBytes());
                         clientOutput.write("<head> <link rel=\"icon\" href=\"data:,\"> </head>\r\n".getBytes());
-                        clientOutput.write(("error 404; resource  " + resource + " not found!\r\n").getBytes());
+                        clientOutput.write(("error 404: resource  " + resource + " not found!\r\n").getBytes());
                         clientOutput.flush();
                     }
 
 
-
                     client.close();
 
+                } catch (NullPointerException e) {
+                    System.out.println("NullPointerException thrown! ln73");
                 }
 
             }
+        } catch (
+                NullPointerException e) {
+            System.out.println("NullPointerException thrown! ln 79");
         }
+    }
+
+    private static String getResourceString(StringBuilder request) {
+        String firstLine = String.valueOf(request.toString().split("\n")[0]);
+        String resource = firstLine.split(" ")[1];
+//        System.out.println("see ongi  resurss " +resource);
+        return resource;
     }
 }
